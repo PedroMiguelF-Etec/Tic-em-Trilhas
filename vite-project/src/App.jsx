f81a05b178527c0018715f0525d2109c5db2fac2
Pedro MiguelF-Etec <pedro.fernandes93@etec.sp.gov.br>
2025-04-02 21:32:20 -0300

    Dia 1: introdução ao React e um pouco da estilisação

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { Cabecalho } from './Components';
+
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Cabecalho />
+    </>
+  )
+};
+
+export { App };
